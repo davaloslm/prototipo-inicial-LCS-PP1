51186490919deaed16b0e5341c5e5e9caa3ef79d
Leonardo Dávalos <davaloslm@hotmail.com.ar>
2026-03-29 03:41:09 -0300

    Feat: Busqueda de envios

---
+++ src/js/envios.js
@@ -0,0 +1,119 @@
+const API_URL = "https://69c692f1f272266f3eaccdd0.mockapi.io/envios";
+ 
+const inputBuscar = document.querySelector("form input[type='text']");
+const selectEstado = document.querySelector("form select");
+const btnBuscar = document.querySelector("form button[type='submit']");
+const btnLimpiar = document.querySelector("form button[type='reset']");
+ 
+const emptyTable = document.getElementById("emptyTable");
+const resultsTable = document.getElementById("resultsTable");
+const noResultsTable = document.getElementById("noResultsTable");
+const tbody = resultsTable.querySelector("tbody");
+
+function actualizarLista(estadoLista) {
+    emptyTable.classList.add("d-none");
+    resultsTable.classList.add("d-none");
+    noResultsTable.classList.add("d-none");
+ 
+    if (estadoLista === "vacia") emptyTable.classList.remove("d-none");
+    if (estadoLista === "resultados") resultsTable.classList.remove("d-none");
+    if (estadoLista === "sinResultados") noResultsTable.classList.remove("d-none");
+}
+ 
+/* function colorearEstado(estado) {
+    const map = {
+        "Pendiente":   { bg: "bg-warning",  text: "text-dark" },
+        "En tránsito": { bg: "bg-primary",  text: "text-white" },
+        "Entregado":   { bg: "bg-success",  text: "text-white" },
+        "Cancelado":   { bg: "bg-danger",   text: "text-white" },
+    };
+    const clases = map[estado] || { bg: "bg-secondary", text: "text-white" };
+    return `<span class="badge rounded-pill ${clases.bg} ${clases.text}">${estado ?? "—"}</span>`;
+} */
+ 
+function listarResultados(envios) {
+    tbody.innerHTML = "";
+ 
+    envios.forEach(envio => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td class="ps-4 small fw-medium">#${envio.trackingId ?? "—"}</td>
+            <td class="small">${envio.destinatario ?? "—"}</td>
+            <td class="small d-md-table-cell text-muted">${envio.direccionEntrega ?? "—"}</td>
+            <td class="small d-none d-md-table-cell text-muted">${envio.fechaEsperada.substring(0,10)}</td>
+            <td><span class="badge rounded-pill bg-warning text-dark">${envio.estado}</span></td>
+            <td><a href="#" class="btn btn-sm btn-outline-secondary">Ver</a></td>
+        `;
+        tbody.appendChild(tr);
+    });
+}
+
+async function buscar() {
+    try {
+        destinatarioOID = document.getElementById('searchInput').value
+        estado = document.getElementById('stateSelect').value
+
+        btnBuscar.disabled = true;
+        btnBuscar.innerHTML = `<span class="spinner-border spinner-border-sm me-1"></span> Cargando...`;
+ 
+        const response = await fetch(API_URL);
+        if (!response.ok) throw new Error("Error al obtener los envíos");
+ 
+        envios = await response.json();
+
+        if (destinatarioOID != "") {
+            if (esID(destinatarioOID)) {
+                console.log("!");
+                envios = envios.filter(envio => envio.trackingId == destinatarioOID)
+            }else{
+                console.log("!!");
+                
+                envios = envios.filter(envio => envio.destinatario == destinatarioOID)
+            }
+
+            
+        }else{
+            if (estado != "Cualquier Estado") {
+                console.log("filtro por estado");
+                
+                envios = envios.filter(envio => envio.estado == "estado 1")
+            }
+        }
+
+        
+ 
+        if (envios.length === 0) {
+            actualizarLista("sinResultados");
+        } else {
+            listarResultados(envios);
+            actualizarLista("resultados");
+        }
+ 
+    } catch (error) {
+        console.error(error);
+        actualizarLista("sinResultados");
+    } finally {
+        btnBuscar.disabled = false;
+        btnBuscar.innerHTML = `<i class="bi bi-search me-1"></i> Buscar`;
+    }
+}
+
+
+
+function esID(param){
+    if (/\d/.test(param)) {
+        return true;        
+    }    
+    return false;
+}
+ 
+
+ 
+btnBuscar.addEventListener("click", function (e) {    
+    e.preventDefault();
+    buscar();
+});
+ 
+btnLimpiar.addEventListener("click", function () {
+    actualizarLista("vacia");
+});
